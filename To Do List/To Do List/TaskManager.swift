94d8697360ee49f3759297581cab1f6305f15cdf
Michael Hamel <hamel.michael.p@gmail.com>
2014-07-26 16:54:09 -0500

    Added TaskManager file.

---
+++ To Do List/To Do List/TaskManager.swift
@@ -0,0 +1,13 @@
+//
+//  TaskManager.swift
+//  To Do List
+//
+//  Created by Michael Hamel on 7/26/14.
+//  Copyright (c) 2014 Michael Hamel. All rights reserved.
+//
+
+import UIKit
+
+class TaskManager: NSObject {
+   
+}
