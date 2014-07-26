7ffcdc900dd6a2721d754aa92bd7f7192014cff4
Michael Hamel <hamel.michael.p@gmail.com>
2014-07-26 16:51:20 -0500

    Added project skeleton.

---
+++ To Do List/To Do List/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  To Do List
+//
+//  Created by Michael Hamel on 7/26/14.
+//  Copyright (c) 2014 Michael Hamel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
